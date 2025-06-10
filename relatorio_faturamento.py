ddc3aae61438e99e271d8253a550c0f8000ef5ed
rodneirac <rodnei.admpuc@gmail.com>
2025-06-10 14:41:36 -0300

    Update relatorio_faturamento.py

--- relatorio_faturamento.py
+++ relatorio_faturamento.py
@@ -6,7 +6,7 @@ import plotly.express as px
 from datetime import datetime
 
 # Logo no topo
-st.image("https://raw.githubusercontent.com/rodneic/relatorio_descontos/main/logo.png", width=200)
+st.image("https://raw.githubusercontent.com/rodneic/BI_Faturamento/main/logo.png", width=350)
 
 # Título
 st.title("ᴄᴏʏᴍᴇ Relatório de Faturamento por Divisão e Mês")
